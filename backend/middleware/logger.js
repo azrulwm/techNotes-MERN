27d6426ae7b6f0f33977d6a75612373975b50598
azrulwm <azrulazmi.wm@gmail.com>
2022-10-31 16:36:46 +0800

    adding errorHanlder, config for cors

--- backend/middleware/logger.js
+++ backend/middleware/logger.js
@@ -20,3 +20,11 @@ const logEvents = async (message, logFileName) => {
     console.log(err);
   }
 };
+
+const logger = (req, res, next) => {
+  logEvents(`${req.method}\t${req.url}\t${req.headers.origin}`, "reqLog.log");
+  console.log(`${req.method} ${req.path}`);
+  next();
+};
+
+module.exports = { logEvents, logger };
