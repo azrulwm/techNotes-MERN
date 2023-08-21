b781434f0aa363d48ae0f92366c2d8597b4d9ee4
azrulwm <azrulazmi.wm@gmail.com>
2023-08-21 12:06:17 +0800

    Fixing issues with notes not having any owner

--- backend/controllers/notesController.js
+++ backend/controllers/notesController.js
@@ -12,7 +12,17 @@ const getAllNotes = asyncHandler(async (req, res) => {
     return res.status(400).json({ message: "No notes found" });
   }
 
-  res.json(notes);
+  // Add username to each note before sending the response
+  // See Promise.all with map() here: https://youtu.be/4lqJBBEpjRE
+  // You could also do this with a for...of loop
+  const notesWithUser = await Promise.all(
+    notes.map(async (note) => {
+      const user = await User.findById(note.user).lean().exec();
+      return { ...note, username: user.username };
+    })
+  );
+
+  res.json(notesWithUser);
 });
 
 // @desc Create new note
