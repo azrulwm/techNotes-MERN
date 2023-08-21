5fa679fb5f01689a5af3efd0091bcc1b36ee9965
azrulwm <azrulazmi.wm@gmail.com>
2023-08-21 11:53:25 +0800

    Limiting content access based on roles

--- frontend/src/components/DashHeader.js
+++ frontend/src/components/DashHeader.js
@@ -1,15 +1,25 @@
 import { useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+import {
+  faFileCirclePlus,
+  faFilePen,
+  faUserGear,
+  faUserPlus,
+  faRightFromBracket,
+} from "@fortawesome/free-solid-svg-icons";
 import { useNavigate, Link, useLocation } from "react-router-dom";
 
 import { useSendLogoutMutation } from "../features/auth/authApiSlice";
+
+import useAuth from "../hooks/useAuth";
 
 const DASH_REGEX = /^\/dash(\/)?$/;
 const NOTES_REGEX = /^\/dash\/notes(\/)?$/;
 const USERS_REGEX = /^\/dash\/users(\/)?$/;
 
 const DashHeader = () => {
+  const { isManager, isAdmin } = useAuth();
+
   const navigate = useNavigate();
   const { pathname } = useLocation();
 
@@ -20,11 +30,10 @@ const DashHeader = () => {
     if (isSuccess) navigate("/");
   }, [isSuccess, navigate]);
 
-  const onLogoutClicked = () => sendLogout();
-
-  if (isLoading) return <p>Logging Out...</p>;
-
-  if (isError) return <p>Error: {error.data?.message}</p>;
+  const onNewNoteClicked = () => navigate("/dash/notes/new");
+  const onNewUserClicked = () => navigate("/dash/users/new");
+  const onNotesClicked = () => navigate("/dash/notes");
+  const onUsersClicked = () => navigate("/dash/users");
 
   let dashClass = null;
   if (
@@ -35,24 +44,91 @@ const DashHeader = () => {
     dashClass = "dash-header__container--small";
   }
 
+  let newNoteButton = null;
+  if (NOTES_REGEX.test(pathname)) {
+    newNoteButton = (
+      <button
+        className="icon-button"
+        title="New Note"
+        onClick={onNewNoteClicked}
+      >
+        {" "}
+        <FontAwesomeIcon icon={faFileCirclePlus} />
+      </button>
+    );
+  }
+
+  let newUserButton = null;
+  if (USERS_REGEX.test(pathname)) {
+    newUserButton = (
+      <button
+        className="icon-button"
+        title="New User"
+        onClick={onNewUserClicked}
+      >
+        <FontAwesomeIcon icon={faUserPlus} />
+      </button>
+    );
+  }
+
+  let usersButton = null;
+  if (isManager || isAdmin) {
+    if (!USERS_REGEX.test(pathname) && pathname.includes("/dash")) {
+      usersButton = (
+        <button className="icon-button" title="Users" onClick={onUsersClicked}>
+          <FontAwesomeIcon icon={faUserGear} />
+        </button>
+      );
+    }
+  }
+
+  let notesButton = null;
+  if (isManager || isAdmin) {
+    if (!NOTES_REGEX.test(pathname) && pathname.includes("/dash")) {
+      notesButton = (
+        <button className="icon-button" title="Notes" onClick={onNotesClicked}>
+          <FontAwesomeIcon icon={faFilePen} />
+        </button>
+      );
+    }
+  }
+
   const logoutButton = (
-    <button className="icon-button" title="Logout" onClick={onLogoutClicked}>
+    <button className="icon-button" title="Logout" onClick={sendLogout}>
       <FontAwesomeIcon icon={faRightFromBracket} />
     </button>
   );
 
+  const errClass = isError ? "errmsg" : "offscreen";
+
+  let buttonContent;
+  if (isLoading) {
+    buttonContent = <p>Logging out...</p>;
+  } else {
+    buttonContent = (
+      <>
+        {newNoteButton}
+        {newUserButton}
+        {notesButton}
+        {usersButton}
+        {logoutButton}
+      </>
+    );
+  }
+
   const content = (
-    <header className="dash-header">
-      <div className={`dash-header__container ${dashClass}`}>
-        <Link to="/dash">
-          <h1 className="dash-header__title">techNotes</h1>
-        </Link>
-        <nav className="dash-header__nav">
-          {/* add more buttons later */}
-          {logoutButton}
-        </nav>
-      </div>
-    </header>
+    <>
+      <p className={errClass}>{error?.data?.message}</p>
+
+      <header className="dash-header">
+        <div className={`dash-header__container ${dashClass}`}>
+          <Link to="/dash">
+            <h1 className="dash-header__title">techNotes</h1>
+          </Link>
+          <nav className="dash-header__nav">{buttonContent}</nav>
+        </div>
+      </header>
+    </>
   );
 
   return content;
